3f3e6af370eea578c62c0b35b36765383d1b6057
abigaildusane <abigaildusane@gmail.com>
2025-10-29 10:33:24 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+from flask import Flask, render_template, request
+from calculator import calculate
 
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    result = ""
+    if request.method == "POST":
+        num1 = request.form["num1"]
+        op = request.form["op"]
+        num2 = request.form["num2"]
+        result = calculate(num1, op, num2)
+    return render_template("index.html", result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
